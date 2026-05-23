995916c355c53bd54b822dd9ab22f835eed61b7b
helloword4381 <jiangflow@foxmail.com>
2026-05-23 21:39:25 +0800

    fix: gradlew auto-bootstrap wrapper jar from Gradle dist

--- android_app/gradlew.bat
+++ android_app/gradlew.bat
@@ -1,11 +1,43 @@
 @echo off
-if "%JAVA_HOME%"=="" (
-    echo ERROR: JAVA_HOME not set. Please set JAVA_HOME to your JDK 17 path.
-    exit /b 1
-)
-
 set DIRNAME=%~dp0
 set APP_HOME=%DIRNAME%
-set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+set WRAPPER_JAR=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+set WRAPPER_PROPS=%APP_HOME%\gradle\wrapper\gradle-wrapper.properties
 
-"%JAVA_HOME%\bin\java.exe" -Dorg.gradle.appname=gradlew -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+:: Find Java
+if "%JAVA_HOME%"=="" (
+    set JAVACMD=java
+) else (
+    set JAVACMD="%JAVA_HOME%\bin\java"
+)
+
+:: Bootstrap: download wrapper jar if not present
+if not exist "%WRAPPER_JAR%" (
+    echo Downloading Gradle wrapper...
+    if exist "%WRAPPER_PROPS%" (
+        for /f "tokens=2 delims==" %%a in ('findstr "^distributionUrl" "%WRAPPER_PROPS%"') do set DIST_URL=%%a
+    )
+    if "%DIST_URL%"=="" set DIST_URL=https://services.gradle.org/distributions/gradle-8.7-bin.zip
+    set DIST_URL=%DIST_URL:\=%
+    
+    :: Create temp dir and download
+    set TMP_DIR=%TEMP%\gradle-bootstrap
+    mkdir "%TMP_DIR%" 2>nul
+    powershell -Command "& {Invoke-WebRequest -UseBasicParsing '%DIST_URL%' -OutFile '%TMP_DIR%\gradle.zip'}"
+    
+    :: Extract wrapper jar
+    powershell -Command "& {Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('%TMP_DIR%\gradle.zip', '%TMP_DIR%\extracted')}"
+    for /r "%TMP_DIR%\extracted" %%f in (gradle-wrapper.jar) do copy "%%f" "%WRAPPER_JAR%" 2>nul
+    
+    :: Cleanup
+    rmdir /s /q "%TMP_DIR%" 2>nul
+    
+    if not exist "%WRAPPER_JAR%" (
+        echo ERROR: Failed to download Gradle wrapper
+        exit /b 1
+    )
+    echo Wrapper jar ready.
+)
+
+%JAVACMD% -Dorg.gradle.appname=gradlew -classpath "%WRAPPER_JAR%" org.gradle.wrapper.GradleWrapperMain %*
+if errorlevel 1 exit /b 1
