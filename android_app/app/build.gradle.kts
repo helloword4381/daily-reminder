644e82274532f41897393adf5fd38319f650347b
helloword4381 <jiangflow@foxmail.com>
2026-05-24 10:13:55 +0800

    bump 1.2.21

--- android_app/app/build.gradle.kts
+++ android_app/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.dailyreminder"
         minSdk = 26
         targetSdk = 34
-        versionCode = 26
-        versionName = "1.2.20"
+        versionCode = 27
+        versionName = "1.2.21"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
