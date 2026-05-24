50f19316def60c48005306f03631c3b7d4484752
helloword4381 <jiangflow@foxmail.com>
2026-05-24 11:56:16 +0800

    bump 1.3.3

--- android_app/app/build.gradle.kts
+++ android_app/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.dailyreminder"
         minSdk = 26
         targetSdk = 34
-        versionCode = 36
-        versionName = "1.3.2"
+        versionCode = 37
+        versionName = "1.3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
